4a4524a513a263925582927f5b4ca67b9c9bd273
elsergio130v <59649728+elsergio130v@users.noreply.github.com>
2020-01-29 21:56:02 +0100

    Add files via upload
    productos

---
+++ MaquinaExpendedora/src/Productos.java
@@ -0,0 +1,30 @@
+public class Productos {
+    public int precioCho = 1500;
+    public int precioMar = 2000;
+    public int precioGa = 1000;
+    public int precioChe = 2000;
+    public int cantidadCho = 20;
+    public int cantidadMar = 30;
+    public int cantidadChe = 10;
+    public int cantidadGa = 10;
+    public int cantidadChoT = 20;
+    public int cantidadMarT = 30;
+    public int cantidadCheT = 10;
+    public int cantidadGaT = 10;
+    public int gananciaMar=0;
+    public int gananciaCho=0;
+    public int gananciaGa=0;
+    public int gananciaChe=0;
+    public int totalCho = 20;
+    public int totalMar = 30;
+    public int totalChe = 10;
+    public int totalGa = 10;
+    public int vendidoCho;
+    public int vendidoMar;
+    public int vendidoChe;
+    public int vendidoGa;
+    public int porcentajeMar;
+    public int porcentajeCho;
+    public int porcentajeGa;
+    public int porcentajeChe;
+}
